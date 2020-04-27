4c3ea263fc5ff0fdd75dded2c74023881411e3c2
vsrs <vit@conrlab.com>
2020-04-27 14:48:27 +0300

    qcow stub comments

---
+++ src/qcow/mod.rs
@@ -0,0 +1,3 @@
+// https://en.wikipedia.org/wiki/Qcow
+// v2,3: https://git.qemu.org/?p=qemu.git;a=blob;f=docs/interop/qcow2.txt
+// v1: http://people.gnome.org/~markmc/qcow-image-format-version-1.html.
